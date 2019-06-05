5f2a85fb267082f35539fa8bc2d7d4d5c62fb458
MatK <42271393+HLRMatK@users.noreply.github.com>
2019-06-05 14:12:54 +0200

    CICD Scoped Test – 1.0.0 (System Administrator) #1 (#2)
    * Update Set XML file of 'CICD Scoped Test – 1.0.0' -- Build #1
    
    * Changes by 'System Administrator' on 'CICD Scoped Test – 1.0.0' -- Build #1

---
+++ sn/CICD Scoped Test/Dictionary Entry/x_70398_cicd_sco_1_scoop/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : CICD Scoped Test
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-06-05 12:01:59
+ * Created By  : admin
+ * Updated On  : 2019-06-05 12:01:59
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=ec3964eedbb133001ac4a565ca961951
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
